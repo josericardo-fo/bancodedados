c75931f485f3020432a1e047e8c3e74386df7bc1
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-07-30 10:48:25 -0300

    update create_tables.sql

--- database-mysql/create-tables.sql
+++ database-mysql/create-tables.sql
@@ -16,19 +16,19 @@ CREATE TABLE Paciente (
 );
 
 CREATE TABLE Medico (
-    CRM varchar(10) PRIMARY KEY,
-    nome varchar(255),
+    CRM varchar(10) PRIMARY KEY nome varchar(255),
     CPF varchar(14),
     id_hospital varchar(18),
     especialidade varchar(100),
     telefone varchar(20),
     email varchar(255),
-    FOREIGN KEY (id_hospital) REFERENCES Hospital(CNPJ)
+    FOREIGN KEY (id_hospital) REFERENCES Hospital(CNPJ),
+    FOREIGN KEY (id_disponibilidade) REFERENCES Disponibilidade(id_medico, id_hospital)
 );
 
 CREATE TABLE Disponibilidade (
-    id_disponibilidade int PRIMARY KEY AUTO_INCREMENT,
     id_medico varchar(10),
+    id_hospital varchar(18),
     dia_semana ENUM(
         'domingo',
         'segunda',
@@ -41,11 +41,12 @@ CREATE TABLE Disponibilidade (
     hr_inicio time,
     hr_fim time,
     disponivel boolean,
-    FOREIGN KEY (id_medico) REFERENCES Medico(CRM)
+    FOREIGN KEY (id_medico) REFERENCES Medico(CRM),
+    FOREIGN KEY (id_hospital) REFERENCES Hospital(CNPJ),
+    PRIMARY KEY (id_medico, id_hospital)
 );
 
 CREATE TABLE Consulta (
-    id_consulta int PRIMARY KEY AUTO_INCREMENT,
     id_medico varchar(10),
     id_hospital varchar(18),
     id_paciente varchar(14),
@@ -54,5 +55,6 @@ CREATE TABLE Consulta (
     consulta_status enum('agendada', 'realizada', 'cancelada'),
     FOREIGN KEY (id_medico) REFERENCES Medico(CRM),
     FOREIGN KEY (id_hospital) REFERENCES Hospital(CNPJ),
-    FOREIGN KEY (id_paciente) REFERENCES Paciente(CPF)
+    FOREIGN KEY (id_paciente) REFERENCES Paciente(CPF),
+    PRIMARY KEY (id_medico, id_hospital, id_paciente)
 );
