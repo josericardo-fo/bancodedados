4905bfea980fb7d9f68af4710d3e6185c28937bc
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-07-30 22:53:50 -0300

    atualizar arquivos e seus nomes

--- database-mongo/insert_mongodb.py
+++ database-mongo/insert_mongodb.py
@@ -1,10 +1,9 @@
 import json
 import sys
 
-# O módulo mongodb_config.py está na pasta config, que é irmão da pasta src e tia da pasta etl
 sys.path.insert(1, "./config")
 sys.path.insert(1, "/data")
-from config_mongo import get_database
+from config_mongodb import get_database
 
 
 def insert_data(collection_name, data):
@@ -15,7 +14,7 @@ def insert_data(collection_name, data):
 
 if __name__ == "__main__":
     # Carregar dados processados
-    with open("data/conversas.json", encoding="utf-8") as f:
+    with open("../data/mongo/historico_conversas.json", encoding="utf-8") as f:
         conversas_data = json.load(f)
 
     insert_data("conversas", conversas_data)
